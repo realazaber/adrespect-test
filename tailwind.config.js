756f09e59eebd3aba7eaa879ce8f06ee094c81ae
AZ <47402666+realazaber@users.noreply.github.com>
2023-08-17 17:42:49 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1b5b31",
+        secondary: "#DCC1AB",
+      },
+    },
+  },
+  plugins: [],
+};
